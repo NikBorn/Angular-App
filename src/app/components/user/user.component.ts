3757c505c6a9fca0444cb983ff255c9544641a71
Nik Bornemeier <NikolausRB@gmail.com>
2018-05-09 15:52:14 -0600

    create user component html file and select template url path

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  template: '<h2>John Doe</h2>'
+  // template: '<h2>John Doe</h2>' //if you want to do inline html
+  templateUrl: './user.component.html'
 })
 
 export class UserComponent {
