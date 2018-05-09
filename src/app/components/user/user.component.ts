263bee79252d9f9ce52e58963ff4b94a2ea1808e
Nik Bornemeier <NikolausRB@gmail.com>
2018-05-09 15:57:55 -0600

    add user component css file

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
+  templateUrl: './user.component.html',
   // template: '<h2>John Doe</h2>' //if you want to do inline html
-  templateUrl: './user.component.html'
+  styleUrls: [ './user.component.css']
+  // styles: [`h2{ color: blue}`] //if you want to do inline css
 })
 
 export class UserComponent {
