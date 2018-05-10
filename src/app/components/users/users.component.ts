488e25a809133702662252fc77008b4b67df2f55
Nik Bornemeier <NikolausRB@gmail.com>
2018-05-10 15:38:02 -0600

    add loop with ngFor

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/User';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
   constructor() { }
 
   ngOnInit() {
+
+    this.users = [
+      {
+        firstName: 'Tyler',
+        lastName: 'Durden',
+        age: 40,
+        address: {
+          street: '2020 Lawrence St.'
+        city: 'Denver',
+          state: 'CO'
+        }
+      },
+      {
+        firstName: 'Brad',
+        lastName: 'Pitt',
+        age: 34,
+        address: {
+          street: '200 School St.'
+        city: 'Denver',
+          state: 'CO'
+        }
+      },
+      {
+        firstName: 'Edward',
+        lastName: 'Norton',
+        age: 27,
+        address: {
+          street: '10 Soap Ave.'
+        city: 'Commerce City',
+          state: 'CO'
+        }
+      }
+
+    ];
+  
+    this.addUser(
+      {
+        firstName: 'Edward',
+        lastName: 'Nono',
+        age: 27,
+        address: {
+          street: '10 Soap Ave.'
+        city: 'Commerce City',
+          state: 'CO'
+        }
+    );
+  }
+
+  addUser(user: User) {
+    this.users.push(user)
   }
 
 }
